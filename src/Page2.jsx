661b42b6bba66312864913c886b426cb3474df46
taofeek-adeniyi <adeniyitaofeek001@gmail.com>
2024-04-26 14:19:56 +0100

    working on card duplicate removal

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -4,6 +4,34 @@ const Page2 = () => {
   const [test, setTest] = useState(`hiiiid`);
   const [isLoading, setIsLoading] = useState(true);
   const [inputArr, setInputArr] = useState();
+  const handleClick = (e) => {
+    // console.log(e.currentTarget.textContent);
+    // const unique = inputArr.filter(
+    //   (items) => items !== e.currentTarget.textContent
+    // );
+    // setInputArr(unique);
+    // const uniqueArray = inputArr.filter(
+    //   (value, index) => inputArr.indexOf(value) === index
+    // );
+    // console.log(unique);
+    // console.log(uniqueArray);
+    // const uniqueElements = new Set();
+    // console.log(uniqueElements);
+    // inputArr.forEach((element) => {
+    //   if (
+    //     element == e.currentTarget.textContent &&
+    //     uniqueElements.has(element)
+    //   ) {
+    //     console.log(e.currentTarget.textContent);
+    //   }
+    // });
+    ///////
+    let count = (inputArr, element) => {
+      return inputArr.filter((ele) => ele === element).length;
+    };
+    let numberOfElement = count(inputArr, e.currentTarget.textContent);
+    console.log(numberOfElement);
+  };
   useEffect(() => {
     const loadedInput = localStorage.getItem("userInput")
       ? localStorage.getItem("userInput")
@@ -25,6 +53,7 @@ const Page2 = () => {
           return (
             <article
               key={index}
+              onClick={handleClick}
               className="shadow-lg hover:shadow-2xl hover:bg-Dark-nude ease-linear duration-300 rounded-md hover:text-white cursor-pointer h-48 w-48 grid place-items-center"
             >
               <div className="uppercase font-bold text-3xl">{item}</div>
