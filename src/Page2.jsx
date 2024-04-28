1df37435a4d1e4a9b005acdfd8b1c08f6c4f3a19
taofeek-adeniyi <adeniyitaofeek001@gmail.com>
2024-04-28 17:20:50 +0100

    refractured code

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -7,52 +7,14 @@ const Page2 = () => {
   const loadedInput = localStorage.getItem("userInput")
     ? localStorage.getItem("userInput")
     : []; // To get the items saved in the local storage
-  const handleClick = (e) => {
-    // console.log(e.currentTarget.textContent);
-    // const unique = inputArr.filter(
-    //   (items) => items !== e.currentTarget.textContent
-    // );
-    // setInputArr(unique);
-    // const uniqueArray = inputArr.filter(
-    //   (value, index) => inputArr.indexOf(value) === index
-    // );
-    // console.log(unique);
-    // console.log(uniqueArray);
-    // const uniqueElements = new Set();
-    // console.log(uniqueElements);
-    // inputArr.forEach((element) => {
-    //   if (
-    //     element == e.currentTarget.textContent &&
-    //     uniqueElements.has(element)
-    //   ) {
-    //     console.log(e.currentTarget.textContent);
-    //   }
-    // });
-    ///////
-    let count = (inputArr, element) => {
-      return inputArr.filter((ele) => ele === element).length;
-    };
-    let numberOfElement = count(inputArr, e.currentTarget.textContent);
-    console.log(numberOfElement);
-    if (numberOfElement > 1) {
-      const unique = inputArr.filter(
-        (items) => items !== e.currentTarget.textContent
-      );
-      // setInputArr(unique);
-
-      //////new test
-      let testArr = Array(numberOfElement).fill(e.currentTarget.textContent);
-      console.log(testArr);
-      // const duplicates = testArr.filter(
-      //   (item, index) => testArr.indexOf(item) !== index
-      // );
-      // console.log(duplicates);
-      // setInputArr(unique.concat(duplicates));
-      /////////////////////////
-      const uniqueElements = (arr) => [...new Set(arr)];
-      setInputArr(unique.concat(uniqueElements(testArr)));
-      // console.log(uniqueElements(testArr));
-    }
+  const handleClick = (idx) => {
+    const uniqueArray = inputArr.filter((item, index, self) => {
+      if (idx === index) {
+        return true;
+      }
+      return item !== self[idx] || index === idx;
+    });
+    setInputArr(uniqueArray);
   };
   useEffect(() => {
     const loadedInput = localStorage.getItem("userInput")
@@ -68,6 +30,9 @@ const Page2 = () => {
   if (isLoading) {
     return <div className="loader"></div>;
   }
+  // if (inputArr.length === 3) {
+  //   alert(`all duplicate removed`);
+  // }
   return (
     <main className="min-h-screen w-screen grid place-items-center">
       <section className="w-full grid place-items-center">
@@ -76,12 +41,12 @@ const Page2 = () => {
             <span className="font-bold text-2xl">Original String: </span>
             <span className="text-2xl uppercase">{loadedInput.toString()}</span>
           </div>
-          {loadedInput.toString() !== inputArr.join("") && (
-            <div className="adjusted-string">
-              <span className="font-bold text-2xl">Adjusted String: </span>
-              <span className="text-2xl uppercase">{inputArr.join("")}</span>
-            </div>
-          )}
+          {/* {loadedInput.toString() !== inputArr.join("") && ( */}
+          <div className="adjusted-string">
+            <span className="font-bold text-2xl">Adjusted String: </span>
+            <span className="text-2xl uppercase">{inputArr.join("")}</span>
+          </div>
+          {/* )} */}
         </div>
       </section>
       {/* {console.log(inputArr)} */}
@@ -90,7 +55,7 @@ const Page2 = () => {
           return (
             <article
               key={index}
-              onClick={handleClick}
+              onClick={() => handleClick(index)}
               className="shadow-lg hover:shadow-2xl hover:bg-Dark-nude ease-linear duration-300 rounded-md hover:text-white cursor-pointer h-48 w-48 grid place-items-center"
             >
               <div className="uppercase font-bold text-3xl">{item}</div>
